7f8ab396359a1366995dd82241dc78cca2c558b4
farid1brahimli <farid1brahimli.bs@gmail.com>
2025-10-24 18:25:22 +0400

    methods task update yarimchiq

---
+++ methods/src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Task1
+//       printGreeting();
+        //Task2
+//        add(2, 3);
+        //Task3
+//        isEven(6);
+    }
+    public static void printTable(int a){
+
+    }
+    //Task3
+//    public  static void isEven(int a){
+//        if(a%2==0){
+//            System.out.println("Even");
+//        } else{
+//            System.out.println("Odd");
+//        }
+//    }
+    //Task2
+//    public static void add(int a,int b){
+//        int sum = a+b;
+//        System.out.println(sum);
+//    }
+    //Task1
+//    public static void printGreeting(){
+//        System.out.println("Hello World");
+//    }
+}
