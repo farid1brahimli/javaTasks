2b4501e1a07e517564370c1559acd9e62b882235
farid1brahimli <farid1brahimli.bs@gmail.com>
2026-02-28 21:41:58 +0400

    thread example code

--- Thread_Task/src/Main.java
+++ Thread_Task/src/Main.java
@@ -1,12 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         Runnable firstTaks = () -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("First Taks" + Thread.currentThread().getName());
         };
 
-        Thread t1 = new Thread(firstTaks);
-        Thread t2 = new Thread(firstTaks);
-        t1.start();
-        t2.start();
+        Runnable secondTask = () -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Second Taks" + Thread.currentThread().getName());
+        };
+
+        Thread firstThread = new Thread(firstTaks);
+        firstThread.setDaemon(true);
+        firstThread.start();
+
+        Thread secondThread = new Thread(secondTask);
+        secondThread.start();
+
+        System.out.println("Main Taks" + Thread.currentThread().getName());
     }
 }
