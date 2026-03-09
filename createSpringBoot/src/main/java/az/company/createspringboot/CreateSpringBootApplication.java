a9089f42e3fc679051bec2f96aca70e9813c77cf
farid1brahimli <farid1brahimli.bs@gmail.com>
2026-03-09 15:05:44 +0400

    SpringBoot application only created

---
+++ createSpringBoot/src/main/java/az/company/createspringboot/CreateSpringBootApplication.java
@@ -0,0 +1,13 @@
+package az.company.createspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateSpringBootApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CreateSpringBootApplication.class, args);
+    }
+
+}
