0c999e07fd04f01a196a72d6f713359af2f50d75
farid1brahimli <farid1brahimli.bs@gmail.com>
2026-03-10 13:05:38 +0400

    spring_jdbc template is added

---
+++ spring_jdbc_template/src/main/java/az/company/spring_jdbc_template/SpringJdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package az.company.spring_jdbc_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcTemplateApplication.class, args);
+    }
+
+}
